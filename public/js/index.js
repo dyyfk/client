311e0104185f2751f303082005d490ccded7976a
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-01-08 23:22:28 +0800

    message form

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,11 @@ var socket = io();
 socket.on('connect',function(){
 	console.log('Connected to server');
 	
-	socket.emit('createMessage', {
-		from: 'jim@example.com',
-		text: 'hey, how is going on'
-	});
+//	socket.emit('createMessage', {
+//		from: 'jim@example.com',
+//		text: 'hey, how is going on'
+//	});
+	
 });
 
 socket.on('disconnect',function(){
@@ -15,4 +16,22 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage', function(message){
 	console.log('New Message', message);
+});
+
+socket.emit('createMessage', {
+	from: 'Frank',
+	text: 'Hello, goodbye'
+},function(serverMessage){
+	console.log('Got it', serverMessage);
+});
+
+jQuery('#message-form').on('submit', function(e){
+	e.preventDefault();
+	
+	socket.emit('createMessage',{
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	},function(){
+		
+	});
 });
