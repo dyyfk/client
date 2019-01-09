ba7dff4fe9d2f9512b63d8bf9cf663e0a99785f5
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-01-10 03:05:13 +0800

    bug fixed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 
-	
 	socket.on('join', (params,callback)=>{
 		if(!isRealString(params.name) || !isRealString(params.room)){
 			return callback('Name and room name are required');
@@ -27,7 +26,7 @@ io.on('connection', (socket) => {
 		
 		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 		
-		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app, Please do not visit the site using old url'));
+		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
   		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', params.name + ' has joined'));
 
 		callback();
@@ -50,8 +49,11 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
 	  var user = users.removeUser(socket.id);
-	  io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-	  io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the room`));
+	  if(user){
+		io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+	  	io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the room`));
+	  }
+
   });
 });
 
