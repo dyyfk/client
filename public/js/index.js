9670741323203e448f5c0b6dc217f3551e280464
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-01-12 16:50:55 +0800

    bug fixed

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ socket.on('updateRoomList',function(rooms){
 	var option = jQuery('<select name="roomJoined"></select>');
 	var displayedRoom = [];
 	var occurence = [];
+	
 	rooms.forEach(function(room){
 		if(!displayedRoom.includes(room)){
 			displayedRoom.push(room);
@@ -18,6 +19,8 @@ socket.on('updateRoomList',function(rooms){
 
 	if(!rooms||rooms.length===0){
 		option.append(jQuery('<option selected disabled hidden></option>').text('No room available'));	
+	}else{
+		option.append(jQuery('<option selected disabled hidden></option>').text('Choose Here'));	
 	}
 	jQuery('#room-list').html(option);
 	
